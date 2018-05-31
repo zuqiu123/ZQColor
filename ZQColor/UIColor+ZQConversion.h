de5c144a25df58f436b1c7da69f6d4b51be86e26
zuqiu123 <815130258@qq.com>
2018-05-31 15:39:58 +0800

    Create First

---
+++ ZQColor/UIColor+ZQConversion.h
@@ -0,0 +1,14 @@
+//
+//  UIColor+conversion.h
+//  MarketManager
+//
+//  Created by zuqiu on 17/5/25.
+//  Copyright © 2017年 finedo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (ZQConversion)
++ (UIColor *) stringToColor:(NSString *)str;
+
+@end
---
+++ ZQColor/UIColor+ZQConversion.m
@@ -0,0 +1,31 @@
+//
+//  UIColor+conversion.m
+//  MarketManager
+//
+//  Created by zuqiu on 17/5/25.
+//  Copyright © 2017年 finedo. All rights reserved.
+//
+
+#import "UIColor+ZQConversion.h"
+
+@implementation UIColor (ZQConversion)
+#pragma mark - 颜色转化
++ (UIColor *) stringTOColor:(NSString *)str
+{
+    if (!str || [str isEqualToString:@""]) {
+        return nil;
+    }
+    unsigned red,green,blue;
+    NSRange range;
+    range.length = 2;
+    range.location = 1;
+    [[NSScanner scannerWithString:[str substringWithRange:range]] scanHexInt:&red];
+    range.location = 3;
+    [[NSScanner scannerWithString:[str substringWithRange:range]] scanHexInt:&green];
+    range.location = 5;
+    [[NSScanner scannerWithString:[str substringWithRange:range]] scanHexInt:&blue];
+    UIColor *color= [UIColor colorWithRed:red/255.0f green:green/255.0f blue:blue/255.0f alpha:1];
+    return color;
+}
+
+@end
